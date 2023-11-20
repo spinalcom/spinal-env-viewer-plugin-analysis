17ce192a1229a537003ececade3ec6b3f5a58135
SSRayane <r.serir@spinalcom.com>
2023-11-20 09:49:05 +0100

    added cron

--- src/vues/dialogs/components/analyticSteps/triggerConfiguration.vue
+++ src/vues/dialogs/components/analyticSteps/triggerConfiguration.vue
@@ -6,85 +6,129 @@
     :md-done.sync="stepper.fourth"
   >
     <md-content class="contents md-scrollbar">
-        <p>
-        A trigger is a condition that must be met for the analytic to be executed.
-        For exemple you can bind on one of the inputs to execute the analytic when
-        the value of the input is changed.
-        Or you can add a time based trigger that will make the analytic execute every 20000 ms ( 20s ).
+      <p>
+        A trigger is a condition that must be met for the analytic to be
+        executed. For exemple you can bind on one of the inputs to execute the
+        analytic when the value of the input is changed. Or you can add a time
+        based trigger that will make the analytic execute every 20000 ms ( 20s
+        ).
+      </p>
 
-        </p>
-
-      <div v-for="([triggerName, value]) in Object.entries(localTriggers)" :key="triggerName" class="block">
-
-        <b>Trigger {{triggerName}}</b>
+      <div
+        v-for="[triggerName, value] in Object.entries(localTriggers)"
+        :key="triggerName"
+        class="block"
+      >
+        <b>Trigger {{ triggerName }}</b>
         <md-field class="fixed-size-field">
           <label>Trigger type</label>
-          <md-select v-model="value.triggerType" @md-selected="resetInfo(value)">
-            <md-option
-              v-for="data of TRIGGER_TYPE"
-              :key="data"
-              :value="data"
-            >{{ data }}</md-option
-            >
+          <md-select
+            v-model="value.triggerType"
+            @md-selected="resetInfo(value)"
+          >
+            <md-option v-for="data of TRIGGER_TYPE" :key="data" :value="data">{{
+              data
+            }}</md-option>
           </md-select>
         </md-field>
 
-        
-        <md-field class="fixed-size-field"  v-if="value.triggerType == TRIGGER_TYPE.INTERVAL_TIME">
+        <md-field
+          class="fixed-size-field"
+          v-if="value.triggerType == TRIGGER_TYPE.INTERVAL_TIME"
+        >
           <label> Interval time value (ms) </label>
-          <md-input type="number" min="0" v-model="value.triggerValue"></md-input>
+          <md-input
+            type="number"
+            min="0"
+            v-model="value.triggerValue"
+          ></md-input>
         </md-field>
-        <md-field class="fixed-size-field" v-if="showInputSelection(value.triggerType)">
-          <label>Input selection (which input do you want to use for change of value tracking)</label>
+        <md-field
+          class="fixed-size-field"
+          v-if="showInputSelection(value.triggerType)"
+        >
+          <label
+            >Input selection (which input do you want to use for change of value
+            tracking)</label
+          >
           <md-select v-model="value.triggerValue">
             <md-option
               v-for="data of Object.keys(inputs)"
               :key="data"
               :value="data"
-            >{{ data }}</md-option
+              >{{ data }}</md-option
             >
           </md-select>
-        </md-field>  
-        <md-field class="fixed-size-field"  v-if="value.triggerType == TRIGGER_TYPE.CHANGE_OF_VALUE_WITH_THRESHOLD">
-          <label> Threshold value  (any change greater than treshold value will trigger analytic) </label>
-          <md-input type="number" min="0" v-model="value.changeOfValueThreshold"></md-input>
+        </md-field>
+        <md-field
+          class="fixed-size-field"
+          v-if="
+            value.triggerType == TRIGGER_TYPE.CHANGE_OF_VALUE_WITH_THRESHOLD
+          "
+        >
+          <label>
+            Threshold value (any change greater than treshold value will trigger
+            analytic)
+          </label>
+          <md-input
+            type="number"
+            min="0"
+            v-model="value.changeOfValueThreshold"
+          ></md-input>
         </md-field>
 
+        <div v-if="value.triggerType == TRIGGER_TYPE.CRON" >
+          
+          <md-button class="md-primary" @click="showCronHelper"> Need help ?
+            <md-icon class="help-icon" @click="showCronHelper"
+              >help</md-icon
+            >
+          </md-button>
+          <md-field
+            class="fixed-size-field"
+            
+          >
+            <label> Cron pattern </label>
+            <md-input
+              v-model="value.triggerValue"
+              placeholder="* * * * *"
+            ></md-input>
+            
+          </md-field>
+        </div>
+        
 
-        <md-button
-          class="md-accent"
-          @click="removeTrigger(triggerName)">
+        <md-button class="md-accent" @click="removeTrigger(triggerName)">
           Remove
         </md-button>
-        
       </div>
 
-      <md-button
-        class="md-primary"
-        @click="addTrigger"
-      >
+      <md-button class="md-primary" @click="addTrigger">
         Add Trigger
       </md-button>
-
+      <cron-help-dialog
+          :showCronHelpModal="showCronHelpModal"
+          @closeCronHelpDialog="showCronHelpModal = false"
+        ></cron-help-dialog>
     </md-content>
   </md-step>
 </template>
 
 <script>
 import { TRIGGER_TYPE } from 'spinal-model-analysis';
-
+import cronHelpDialog from '../cronHelpDialog.vue';
 
 export default {
-  props: ['STEPPERS_DATA', 'stepper'
-  ,'inputs', 'triggers' ],
+  props: ['STEPPERS_DATA', 'stepper', 'inputs', 'triggers'],
   components: {
+    'cron-help-dialog':cronHelpDialog,
   },
   data() {
     return {
-        localTriggers: this.triggers,
-        showPreviewDialog:false,
-        previewData: '',
-      
+      localTriggers: this.triggers,
+      showPreviewDialog: false,
+      showCronHelpModal: false,
+      previewData: '',
     };
   },
   created() {
@@ -95,34 +139,35 @@ export default {
       console.log('clicked on button add Trigger');
       this.$emit('addTrigger');
     },
-    removeTrigger(triggerName){
-      this.$emit('removeTrigger',triggerName);
+    removeTrigger(triggerName) {
+      this.$emit('removeTrigger', triggerName);
     },
-    showInputSelection(triggerType){
-      return [TRIGGER_TYPE.CHANGE_OF_VALUE_WITH_THRESHOLD, TRIGGER_TYPE.CHANGE_OF_VALUE].includes(triggerType);
+    showInputSelection(triggerType) {
+      return [
+        TRIGGER_TYPE.CHANGE_OF_VALUE_WITH_THRESHOLD,
+        TRIGGER_TYPE.CHANGE_OF_VALUE,
+      ].includes(triggerType);
     },
 
-    resetInfo(triggerInfo){
+    showCronHelper() {
+      this.showCronHelpModal = true;
+      console.log('showCronHelper : ', this.showCronHelpModal);
+    },
+
+    resetInfo(triggerInfo) {
       console.log('reset info');
       triggerInfo.changeOfValueThreshold = null;
       triggerInfo.triggerValue = '';
     },
-
-
   },
-  computed: {
-    
-    
+  computed: {},
 
-    
-  },
-
-  watch:{
-    triggers(){
-      console.log('parent triggers changed')
+  watch: {
+    triggers() {
+      console.log('parent triggers changed');
       this.localTriggers = this.triggers;
-    }
-  }
+    },
+  },
 };
 </script>
 
@@ -133,7 +178,8 @@ export default {
   margin-bottom: 10px;
   width: 90%;
 }
-
-
+.help-icon {
+  cursor: pointer;
+  margin-left: 10px;
+}
 </style>
-
---
+++ src/vues/dialogs/components/cronHelpDialog.vue
@@ -0,0 +1,86 @@
+<template>
+  <md-dialog
+    :md-click-outside-to-close="false"
+    :md-close-on-esc="false"
+    class="mdDialog"
+    :md-active.sync="showCronHelpModal"
+  >
+    <md-dialog-title>Cron Pattern Guide</md-dialog-title>
+    <md-dialog-content>
+      <p
+        >This guide will help you understand how to create cron patterns for
+        scheduling tasks. A cron pattern is composed of five fields separated by a blank space: (minute) (hour) (day of the month) (month) (day of the week)</p
+      >
+      <ul>
+        <li><strong>Minute</strong> (0 - 59)</li>
+        <li><strong>Hour</strong> (0 - 23)</li>
+        <li><strong>Day of the Month</strong> (1 - 31)</li>
+        <li><strong>Month</strong> (1 - 12 or JAN - DEC)</li>
+        <li
+          ><strong>Day of the Week</strong> (0 - 7, where 0 or 7 is Sunday, or
+          SUN - SAT)</li
+        >
+      </ul>
+      <p>A field may be an asterisk (*), which always stands for first-last</p>
+
+      <p
+        >Ranges of numbers are allowed. Ranges are two numbers separated with a
+        hyphen. The specified range is inclusive. For example, 8-11 for an
+        ''hour'' entry specifies execution at hours 8, 9, 10 and 11.</p
+      >
+
+      <p
+        >Lists are allowed.  A list is a set of numbers (or ranges)
+             separated by commas.  Examples: ``1,2,5,9'', ``0-4,8-12''.</p
+      >
+
+      <p>Here are some common examples:</p>
+      <ul>
+        <li><code>0 * * * *</code> - Every hour at the start of the hour.</li>
+        <li><code>30 9 * * 1</code> - Every Monday at 9:30 AM.</li>
+        <li
+          ><code>0 0 1 * *</code> - The first day of every month at
+          midnight.</li
+        >
+      </ul>
+    </md-dialog-content>
+    <md-dialog-actions>
+      <md-button class="md-primary" @click="closeDialog()">Close</md-button>
+    </md-dialog-actions>
+  </md-dialog>
+</template>
+
+<script>
+export default {
+  props: ['showCronHelpModal'],
+  data() {
+    return {};
+  },
+  methods: {
+    closeDialog() {
+      this.$emit('closeCronHelpDialog');
+    },
+  },
+};
+</script>
+
+<style scoped>
+.mdDialog {
+  z-index: 1000 !important;
+  width: 100%;
+  padding: 20px;
+  border-radius: 10px;
+}
+
+.md-dialog-content ul {
+  padding-left: 20px;
+  list-style-type: disc;
+}
+.md-dialog-content code {
+  background-color: #e5e5e5;
+  margin-bottom: 5px;
+  padding: 2px 4px;
+  border-radius: 4px;
+  font-family: monospace;
+}
+</style>
